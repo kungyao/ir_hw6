d3875d7576bfd747212c35d1de79d331db26204d
KungYao <masa10269@gmail.com>
2020-12-28 17:01:24 +0800

    print

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from model import get_albert_model_and_tokenizer
 
 def get_args():
     parser = argparse.ArgumentParser(description='', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-ep', '--epoch', type=int, default=15, dest='epoch')
+    parser.add_argument('-ep', '--epoch', type=int, default=5, dest='epoch')
     parser.add_argument('-bs', '--batch_size', type=int, default=1, dest='batch_size')
     parser.add_argument('-o', '--output', type=str, default='./models', dest='output')
     return parser.parse_args()
@@ -36,7 +36,7 @@ if __name__ == '__main__':
     trainset = CorpusSet(tokenizer, docs, train, mode='train')
     trainloader = DataLoader(trainset, batch_size=args.batch_size, collate_fn=collate_fn, shuffle=True)
 
-    print_thres = 100 - 1
+    print_thres = 100
     model.to(device)
     for ep in range(args.epoch):
         print(f'training epoch {ep}')
@@ -56,8 +56,11 @@ if __name__ == '__main__':
             total_loss += res.loss
             # loss = res.loss
             # logits = res.logits
-            if i != 0 and i % print_thres == 0:
-                print(f'epoch {ep}, batch {i+1}, loss {total_loss / print_thres}')
+            if i % print_thres == 0:
+                if i == 0:
+                    print(f'epoch {ep}, batch {i}, loss {total_loss}')
+                else:
+                    print(f'epoch {ep}, batch {i}, loss {total_loss / print_thres}')
                 total_loss = 0
         torch.save(model, os.path.join(args.output, f'epoch_{ep}.mdl'))
 
